f200066ceb66a92a3f0966dec3808d006685e5ca
raphactn <raphael.d.caetano48@gmail.com>
2023-04-26 21:54:37 -0300

    atualização projeto rick and morty

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,59 @@
-import Image from "next/image"
+import Image from "next/image";
+import { Flex, Heading, Box, Text, IconButton } from "@chakra-ui/react";
+import { AiOutlineHeart } from "react-icons/ai";
 
-export const Card = ({personagem}) => {
-    return (
-        <div>
-            <Image src={personagem.image} width={'300'} height={'300'}/>
-            <h1>{personagem.name}</h1>
-        </div>
-    )
-}
+export const Card = ({ personagem }) => {
+  let arrayId = [];
+
+  const handleFavorit = (id) => {
+    arrayId = JSON.parse(localStorage.getItem("favorits")) || [];
+    if (arrayId.includes(id)) {
+      arrayId.splice(arrayId.indexOf(id), 1);
+      localStorage.setItem("favorits", JSON.stringify(arrayId));
+    } else {
+      arrayId.push(id);
+      localStorage.setItem("favorits", JSON.stringify(arrayId));
+    }
+  };
+
+  return (
+    <>
+      <Flex
+        gap={3}
+        bg="gray.100"
+        rounded={"md"}
+        justifyContent={"space-between"}
+      >
+        <Flex>
+          <Image
+            src={personagem.image}
+            width={"200"}
+            height={"200"}
+            alt={personagem.name}
+          />
+          <Box p={5}>
+            <Heading size="md" mb={5}>
+              {personagem.name}
+            </Heading>
+            <Text>{personagem.species}</Text>
+            <Flex gap={2} alignItems={"center"}>
+              <Text>{personagem.status}</Text>
+              <Box
+                w="10px"
+                h="10px"
+                rounded={"full"}
+                bg={personagem.status === "Alive" ? "green.500" : "red.500"}
+              />
+            </Flex>
+            <Text>{personagem.location.name}</Text>
+          </Box>
+        </Flex>
+        <IconButton
+          variant={"ghost"}
+          onClick={() => handleFavorit(personagem.id)}
+          icon={<AiOutlineHeart fontSize={"20px"} />}
+        />
+      </Flex>
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,105 @@
+import {
+  Container,
+  SimpleGrid,
+  Input,
+  Select,
+  Flex,
+  Image,
+  Center,
+  IconButton,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 import { Card } from "@/components/Card";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 const home = () => {
   const [results, setResults] = useState([]);
-  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
+  const [page, setPage] = useState(1);
+  const [info, setInfo] = useState(0);
+  const [activeFavorits, setActiveFavorits] = useState(false);
 
   useEffect(() => {
     axios
-      .get("https://rickandmortyapi.com/api/character")
+      .get(
+        activeFavorits
+          ? `https://rickandmortyapi.com/api/character/${localStorage["favorits"]}`
+          : `https://rickandmortyapi.com/api/character/?page=${page}&name=${search}&status=${status}`
+      )
       .then((res) => {
-        setResults(res.data.results);
+        if (res.data.results || res.data.info) {
+          setInfo(res.data.info.pages);
+          setResults(res.data.results);
+        } else {
+          setInfo(0);
+          setResults(res.data);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [search, status, page, activeFavorits]);
 
-  console.log(results);
+  //Metodo de pesquisa de array.
+
+  // const filterResults = results.filter((item) => {
+  //   const name = item.name.toLocaleLowerCase();
+  //   if (status) {
+  //     return (
+  //       name.includes(search.toLocaleLowerCase()) && item.status === status
+  //     );
+  //   }
+  //   return name.includes(search.toLocaleLowerCase());
+  // });
+
   return (
-    <div>
-      {results.map((item) => (
-        <Card key={item.id} personagem={item}/>
-      ))}
-    </div>
+    <Container maxW={"1400px"} p={10}>
+      <Center>
+        <Image src="/logo.png" w={"100%"} maxW={"400"} />
+      </Center>
+      <Flex gap={2} alignItems={"center"}>
+        <Input
+          my={10}
+          placeholder="Search characters..."
+          type="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Select
+          placeholder="selecione"
+          maxW="200px"
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <option value="alive">alive</option>
+          <option value="dead">dead</option>
+          <option value="unknown">unknown</option>
+        </Select>
+        <Button onClick={() => setActiveFavorits(!activeFavorits)}>
+          Favoritos
+        </Button>
+      </Flex>
+      <SimpleGrid columns={2} spacing={5}>
+        {results.map((item) => (
+          <Card key={item.id} personagem={item} />
+        ))}
+      </SimpleGrid>
+
+      <Flex gap={2} my={5} justifyContent={"end"} alignItems={"center"}>
+        <Text mr="10">{`Page ${page} de ${info}`}</Text>
+        <IconButton
+          isDisabled={page === 1}
+          icon={<ChevronLeftIcon />}
+          onClick={() => setPage(page - 1)}
+        />
+        <IconButton
+          icon={<ChevronRightIcon />}
+          onClick={() => setPage(page + 1)}
+        />
+      </Flex>
+    </Container>
   );
 };
 export default home;
